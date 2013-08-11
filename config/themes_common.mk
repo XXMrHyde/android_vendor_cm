a584b5b7e480245039df6a64974e402e5aef5159
XXMrHyde <xxmrhyde@gmx.co.uk>
2013-08-12 00:49:17 +0200

    Build AlphaBlueModV2(Light) Theme from source
    Change-Id: Ic4f23b7e17fa9787cb32c8c08992cd7eee8930fc

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -2,7 +2,9 @@
 PRODUCT_PACKAGES += \
        ThemeManager \
        ThemeChooser \
-       com.tmobile.themes
+       com.tmobile.themes \
+	   AlphaBlueModV2 \
+       AlphaBlueModV2Light
 
 PRODUCT_COPY_FILES += \
        vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
