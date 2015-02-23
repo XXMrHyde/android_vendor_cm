7f6817fb4d0b6289e6d9568575fee17e31bed954
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-02-23 16:41:16 +0100

    Change default sounds, (1/2)
    Change-Id: I2e763a30dd26ea857bcc13560e93382cf91be74d

--- config/telephony.mk
+++ config/telephony.mk
@@ -20,7 +20,3 @@ PRODUCT_PACKAGES += \
 # Mms depends on SoundRecorder for recorded audio messages
 PRODUCT_PACKAGES += \
     SoundRecorder
-
-# Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Orion.ogg
